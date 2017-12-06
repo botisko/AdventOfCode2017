12b6d71ee5f0fa424a8a3f1b2dd28c59a2e49c52
botisko <34273686+botisko@users.noreply.github.com>
2017-12-06 15:21:28 +0100

    Day 01 - Part I & II initial commit

---
+++ 01a.py
@@ -0,0 +1,31 @@
+"""Advent of Code - Day 01 - Reverse Captcha - Part I"""
+
+import sys
+
+def advent01():
+    """Open TXT input and count the captcha sum"""
+    f = open(sys.argv[1],"r")
+
+    pole = f.readline()
+
+    totalSum = 0
+    n = 0
+
+    # Run through the input and do the countning
+    while n < (len(pole)-2):
+        if int(pole[n]) == int(pole[n+1]):
+            totalSum = totalSum + int(pole[n])
+
+        # print("Index:",n,"Actual:",pole[n],"Next:",pole[n+1],"Actual sum:",totalSum)
+        n = n+1;
+
+    # Compare the 1st and last ("circle buffer")
+    if int(pole[0]) == int(pole[-2]):
+        totalSum = totalSum + int(pole[0])
+
+    print("The inverse captcha sum of the input is:",totalSum)
+
+    f.close()
+
+if __name__ == '__main__':
+    advent01()
---
+++ 01b.py
@@ -0,0 +1,28 @@
+"""Advent of Code - Day 01 - Reverse Captcha - Part II"""
+
+import sys
+
+def advent01():
+    """Open TXT input and count the captcha sum"""
+    f = open(sys.argv[1],"r")
+
+    pole = f.readline()
+
+    totalSum = 0
+    n = 0
+    halfPole = int((len(pole)-1)/2)
+
+    # Run through the input and do the countning
+    while n < (halfPole):
+        if int(pole[n]) == int(pole[n+halfPole]):
+            totalSum = totalSum + int(pole[n]) + int(pole[n+halfPole])
+
+        print("Index A:",n,"Actual:",pole[n],"Index N:",n+halfPole,"Next:",pole[n+halfPole],"Actual sum:",totalSum)
+        n = n+1;
+
+    print("The inverse captcha sum of the input is:",totalSum)
+
+    f.close()
+
+if __name__ == '__main__':
+    advent01()
